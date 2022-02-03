15a15e0eada2740ebce7eea8d0e9067dde543d9e
lialittis <yu-tianchi@outlook.com>
2022-02-04 00:09:44 +0100

    add Model1v1.2 with a coming idea that re-construct a list of special type value for each task

---
+++ src/Model1v1.2/Parser.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+class ParserForRequirements():
+    # open nominal files
+    # example of one nominal file
+    def __init__(self, filename):
+        """
+        Initializes the parser
+        """
+        self.filename = filename
+        # self.lines = []
+        self.header =["Task number","Satellite","Priority","Duration","Earliest","Latest","Repetitive","Number occ","Min time lag","Max time lag"]
+    
+    def open_file(self):
+        with open(self.filename) as f:
+            lines = f.readlines()
+        return lines;
+    
+    def read_list_sat(self):
+        #'./PIE_SXS10_data/nominal/scenario_10SAT_nominal1.txt'
+        self.lines = self.open_file()
+        list_sat = self.lines[1].strip().split('\t')
+        return list_sat
+
+    def get_requirements_data(self):
+        # requirements for each task
+        self.lines = self.open_file()
+        data_i = []
+        for i in range(3,len(self.lines)):
+            data_i.append(self.lines[i].strip().split())
+        # print(data_i)
+
+        data_df = pd.DataFrame(data_i,columns=self.header)
+        data_df[["Priority","Duration","Earliest",
+                "Latest","Repetitive","Number occ",
+                "Min time lag","Max time lag"]] = data_df[["Priority","Duration","Earliest",
+                                                            "Latest","Repetitive","Number occ",
+                                                            "Min time lag","Max time lag"]].astype(int)
+        return data_df
+
+class ParserForVisibilities():
+    # open visibility files
+    def __init__(self, filename):
+        """
+        Initializes the parser
+        """
+        self.filename = filename
+        #self.visibs = []
+        self.header_visibs = []
+        self.data_visibs = []
+    
+    def open_file(self):
+        with open(self.filename) as f:
+            self.visibs = f.readlines()
+        return self.visibs;
+
+    def get_visibs_data(self):
+        self.visibs = self.open_file()
+        for i in range(len(self.visibs)):
+            if i == 0:
+                self.header_visibs = self.visibs[i].strip().split()
+            else:
+                self.data_visibs.append(self.visibs[i].strip().split())
+        data_visib_df = pd.DataFrame(self.data_visibs,columns=self.header_visibs)
+        data_visib_df[["Start","End"]] = data_visib_df[["Start","End"]] .astype(int)
+        return data_visib_df
+
+
+    
+
+
+
---
+++ src/Model1v1.2/main.py
@@ -0,0 +1,112 @@
+import sys
+import collections
+from ortools.sat.python import cp_model
+from Parser import ParserForRequirements as pfr
+from Parser import ParserForVisibilities as pfv
+from helper import *
+from ModelCP import *
+from checker import CheckerInOne as check
+
+def ModelSimple(req_file = './../../PIE_SXS10_data/nominal/scenario_10SAT_nominal_example.txt',
+                visib_file = './../../PIE_SXS10_data/visibilities_test.txt'):
+    parser_req = pfr(req_file)
+    parser_visib = pfv(visib_file)
+
+    data_df = parser_req.get_requirements_data()
+    data_visib_df = parser_visib.get_visibs_data()
+
+    # parameters
+    n_tasks = len(data_df['Task number'])
+    list_sats = [int(i.strip('SAT')) for i in data_visib_df.Sat.unique().tolist()]
+    list_antennes = [int(i.strip('ANT')) for i in data_visib_df.Ant.unique().tolist()]
+    n_antennes = len(list_antennes)
+
+    print("@Requirements are: \n",data_df)
+    print("@Visibilities are: \n",data_visib_df)
+    print("@list of antennes: \n", list_antennes)
+
+    # construct objects of Ant
+    d_ants = {}
+    all_sats = []
+    dict_visib = {}
+
+    print("\n==>START MODEL<==\n")
+    # Creates the model.
+    # [START model]
+    model = cp_model.CpModel()
+    # [END model]
+
+    for i in range(len(data_visib_df['Ant'])) :
+        name = data_visib_df['Ant'][i]
+        ID = int(name.strip('ANT'))
+        start = int(data_visib_df['Start'][i])
+        end = int(data_visib_df['End'][i])
+        if ID in d_ants:
+            count = len(d_ants[ID])
+            d_ants[ID].append(model.NewIntervalVar(start,end-start,end,str(count+1)))
+        else:
+            d_ants[ID] = []
+            d_ants[ID].append(model.NewIntervalVar(start,end-start,end,str(1)))
+
+    # construct the matrix for visib
+    # initialize
+    for i in range(len(data_visib_df)):
+        dict_visib[data_visib_df["Sat"][i],data_visib_df["Ant"][i]] = []
+
+    print("-->Initialization for the matrix of visibility<--")
+    # print(dict_visib)
+    # add element
+    for i in range(len(data_visib_df)):
+        dict_visib[data_visib_df["Sat"][i],data_visib_df["Ant"][i]].append((data_visib_df['Start'][i],data_visib_df['End'][i]))
+
+    print("-->FINISED<--")
+    # print("After add elements : \n", dict_visib)
+
+    dict_non_visib = get_dict_non_visib(dict_visib)
+
+    # change keys for dict_non_visib
+    oldkeys = dict_non_visib.keys()
+    for sat,ant in list(oldkeys):
+        sat_id = int(sat.replace("SAT",""))
+        ant_id = int(ant.replace("ANT",""))
+        new_key = (sat_id,ant_id)
+        dict_non_visib[new_key] = dict_non_visib.pop((sat,ant))
+
+    print("\n-->ARGUMENTS<--")
+    # print("NON visibility of sat : \n", dict_non_visib)
+
+    # transfer dataframe of requirements and visibilities dataframe to dictionary
+    dict_data_df = data_df.to_dict()
+    dict_visib_df = data_visib_df.to_dict()
+    print("@Requirements dictionary : \n",dict_data_df)
+    # print("@Visibility dictionary : \n", dict_visib_df)
+
+    print("-->FINISHED<--\n")
+    results = SimpleSatProgram(model,dict_data_df,dict_non_visib,n_tasks,list_antennes)
+    print("==>END MODEL<==\n")
+    return dict_data_df,list_sats,list_antennes,results
+
+def ModelNominalV1(req_file,visib_file='./../../PIE_SXS10_data/visibilities.txt'):
+    return ModelSimple(req_file,visib_file)
+
+if __name__ == '__main__':
+    arguments = sys.argv
+    print(arguments)
+    dict_res = {}
+    dict_req = {}
+    list_sats = []
+    list_ants = []
+    if len(arguments) == 1:
+        dict_req,list_sats,list_ants,dict_res = ModelSimple()
+    elif len(arguments) == 2:
+        dict_req,list_sats,list_ants,dict_res = ModelNominalV1(arguments[1])
+    elif len(arguments) == 3:
+        dict_req,list_sats,list_ants,dict_res = ModelNominalV1(arguments[1],arguments[2])
+    else:
+        print("Wrong arguments")
+    print("==>START CHECKING<==")
+    # print(dict_req)
+    print("-->results are<--")
+    print(dict_req)
+    check(dict_req,list_sats,list_ants,dict_res)
+    print("==>END CHECKING<==")
